20d7e2691f065e44897c1413c89bee08c5ab3519
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2021-11-16 00:21:25 +0100

    Add transition effect to theme change

--- assets/dark-mode.js
+++ assets/dark-mode.js
@@ -1,11 +1,19 @@
 document.addEventListener('keydown', function(event){
     if (event.ctrlKey && event.key==' '){
         const theme = window.matchMedia("(prefers-color-scheme: dark)");
+        
+        document.documentElement.classList.add('color-theme-in-transition');
+        window.setTimeout(() => {
+            document.documentElement.classList.remove('color-theme-in-transition');
+        }, 1000);
 
         if (theme.matches){
             document.documentElement.classList.toggle('light-theme')
         } else{
             document.documentElement.classList.toggle('dark-theme');
-        }     
+        }
+           
     }
 });
+
+
